a88e95200950ee041497f904800f6bb9e857f734
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-09-10 14:27:30 +0300

    from fleet

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hli%gh91b6voaommdymmicwar*hgg7=6*b8*rj-bdt8s43p0r='
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-hli%gh91b6voaommdymmicwar*hgg7=6*b8*rj-bdt8s43p0r=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", False)) == True
 
 ALLOWED_HOSTS = []
-
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get("ALLOWED_HOSTS")]
 
 # Application definition
 
